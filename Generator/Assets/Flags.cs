ff72df3e6d968bfffcbf3120356c27b18fe685be
lunarsoap5 <justindakotacarter@gmail.com>
2022-04-08 15:11:38 -0500

    Began adding cutscene flags to be skipped

--- Generator/Assets/Flags.cs
+++ Generator/Assets/Flags.cs
@@ -62,6 +62,43 @@ namespace TPRandomizer.Assets
                 { 0x0, 0x63 }, // Spawn the Chest in Link's House
                 { 0x2, 0x4B }, // Unlock North Faron Woods Gate
             };
+        
+        public static readonly byte[,] CutsceneRegionFlags = new byte[,]
+            {
+                { 0x0, 0x4A }, // Ordon Day 3 Intro CS.
+                { 0x0, 0x4C }, // Knocked down Ordon bee nest CS.
+                { 0x0, 0x4E }, // Ordon Ranch first time CS.
+                { 0x0, 0x53 }, // Ilia spring CS watched.
+                { 0x0, 0x54 }, // Ilia spring CS started.
+                { 0x0, 0x55 }, // Ordon Village first time CS.
+                { 0x0, 0x56 }, // Ilia spring CS trigger.
+                { 0x0, 0x68 }, // Approach Faron Twilgiht with Midna CS.
+                { 0x0, 0x6E }, // Enter shield house as wolf CS.
+                { 0x0, 0x75 }, // Midna text after hearing Bo and Jaggle talk about the shield.
+                { 0x0, 0x7C }, // Midna text before jumping to Ordon Shop roof.
+                { 0x0, 0x7D }, // Rusl talking to Uli during wolf night CS.
+                { 0x0, 0xB8 }, // Enter Ordon Village as wolf CS.
+                { 0x1, 0x42 }, // Midna text after first gate in sewers.
+                { 0x1, 0x43 }, // Midna text after exiting to rooftops.
+                { 0x1, 0x44 }, // Woke up in jail CS.
+                { 0x1, 0x4B }, // Midna CS after digging out of jail.
+                { 0x1, 0x4C }, // Midna intro CS.
+                { 0x1, 0x51 }, // Zelda tower intro CS.
+                { 0x1, 0x57 }, // Outside top door intro CS.
+                { 0x1, 0x5A }, // Went to the otherside of the fence in sewers CS.
+                { 0x1, 0x5B }, // Top of stairway intro CS.
+                { 0x1, 0x5C }, // Stairway intro CS.
+                { 0x1, 0x7B }, // Midna text when approaching the rooftop guard.
+                { 0x1, 0x7F }, // Midna CS after digging out of jail. (Second flag)
+                { 0x2, 0x74 }, // Faron intro CS.
+                { 0x2, 0x77 }, // See Faron Light Spirit from afar CS.
+                { 0x2, 0x7C }, // Entered mist area as human.
+                { 0x2, 0x95 }, // Midna text after warping to North Faron for bridge.
+                { 0x3, 0x49 }, // Death mountain intro CS.
+                { 0x3, 0x83 }, // Kakariko Graveyard intro CS.
+                { 0x3, 0x8C }, // Midna text after Meteor fell.
+                { 0x3, 0x9A }, // Kakariko Village intro CS.
+            };
 
         /// <summary>
         /// summary text.
@@ -71,6 +108,7 @@ namespace TPRandomizer.Assets
             { 0, IntroRegionFlags },
             { 1, FaronTwilightRegionFlags },
             { 2, EldinTwilightRegionFlags },
+            { 3, CutsceneRegionFlags },
         };
 
         /// <summary>
@@ -129,6 +167,31 @@ namespace TPRandomizer.Assets
             { 0x12, 0x8 }, // Can use Sera's Shop.
         };
 
+        public static readonly byte[,] CutsceneEventFlags = new byte[,]
+        {
+            { 0x1, 0x40 }, // Talked to Yeto First Time.
+            { 0x3, 0x90 }, // Jaggle Calls out to Link, talked to Squirrel as Wolf in Ordon.
+            { 0x5, 0x10 }, // Unchain Wolf Link.
+            { 0x6, 0xC0 }, // CS After beating Ordon Shadow, CS after entering Faron Twilight.
+            { 0x10, 0x2 }, // Talked to Jaggle after climbing vines.
+            { 0xF, 0x40 }, // Talked to Doctor for the first time.
+            { 0x5E, 0x10 }, // Midna text after beating Forest Temple.
+            { 0x1D, 0x40 }, // Listened to Fyer at drained lake.
+            { 0x22, 0x1 }, // Plumm initial CS watched.
+            { 0x26, 0x2 }, // Talked to Yeto on Snowpeak.
+            { 0x28, 0x40 }, // Used Ooccoo for the first time.
+            { 0x37, 0x4 }, // Postman twilight text.
+            { 0x38, 0x6 }, // Hena cabin first time CS, talked to Hena first time.
+            { 0x3A, 0x1 }, // Talked to Ralis in Graveyard for the first time.
+            { 0x40, 0x2 }, // Agreed to help Rusl after Snowpeak Ruins.
+            { 0x42, 0x1 }, // Watched post-ToT Ooccoo CS.
+            { 0x45, 0x8 }, // Allows postman letters to show up in inventory.
+            { 0x4A, 0x10 }, // Saw Talo in cage CS.
+            { 0x3E, 0x2 }, // City Ooccoo CS watched.
+            { 0x59, 0x40 }, // Met Postman for the first time.
+            { 0x5D, 0x40 }, // Midna text after Kargarok flight.
+        };
+
         /// <summary>
         /// summary text.
         /// </summary>
@@ -137,6 +200,7 @@ namespace TPRandomizer.Assets
             { 0, IntroEventFlags },
             { 1, FaronTwilightEventFlags },
             { 2, EldinTwilightEventFlags },
+            { 3, CutsceneEventFlags },
         };
         private static readonly RandomizerSetting RandomizerSettings = Randomizer.RandoSetting;
         /// <summary>
@@ -147,6 +211,7 @@ namespace TPRandomizer.Assets
             RandomizerSettings.introSkipped,
             RandomizerSettings.faronTwilightCleared,
             RandomizerSettings.eldinTwilightCleared,
+            RandomizerSettings.skipMinorCutscenes,
         };
     }
 }
